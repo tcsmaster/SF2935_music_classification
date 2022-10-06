c975fe95719d86d93dcad31cfe5d80895b68b428
ertam98 <etamm@kth.se>
2022-10-06 16:02:14 +0200

    Implemented QDA

---
+++ qda.py
@@ -0,0 +1,19 @@
+import pandas as pd
+from sklearn.discriminant_analysis import QuadraticDiscriminantAnalysis
+from sklearn.model_selection import cross_val_score
+
+df_train = pd.read_csv('data/project_train.csv', header = 0)
+df_test = pd.read_csv('data/project_test.csv', header = 0)
+
+df_train_X = df_train.iloc[:,0:11]
+df_train_y = df_train.iloc[:,11]
+
+qda = QuadraticDiscriminantAnalysis()
+qda.fit(df_train_X, df_train_y)
+
+# Cross_validation
+scores = cross_val_score(qda, df_train_X, df_train_y, cv=5)
+print('Accuracy: %.02f, Stdev: %.02f' %(scores.mean(), scores.std()))
+
+y_pred = qda.predict(df_test)
+print(y_pred)
